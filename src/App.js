72fcabc4e7dae41ca59a9a5cb761eaaed375e8b0
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2022-05-15 12:53:18 +0300

    replace filter & contacts logic to <Contacts />
    replace filter & contacts logic to <Contacts />

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.styled.jsx';
 import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
-import { deleteContact, getContacts, getFilter } from 'redux/contactSlice';
 import { Container } from 'App.styled';
 import { MyForm } from 'components/Form/Form.jsx';
 import { Contacts } from 'components/Contacts/Contacts.jsx';
@@ -9,32 +7,13 @@ import { Filter } from 'components/Filter/Filter.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
-  const handleDelete = e => {
-    const chengedContacts = contacts.filter(
-      contact => contact.id !== e.currentTarget.parentElement.id
-    );
-    dispatch(deleteContact(chengedContacts));
-  };
-
-  const filteredContacts = contacts.filter(contact => {
-    return contact.name
-      .toLocaleLowerCase()
-      .includes(filter.toLocaleLowerCase());
-  });
   return (
     <Container>
       <h1>Phonebook</h1>
       <MyForm />
       <h2>Contacts:</h2>
       <Filter />
-      <Contacts
-        contacts={filter !== '' ? filteredContacts : contacts}
-        onDelete={handleDelete}
-      ></Contacts>
+      <Contacts />
     </Container>
   );
 };
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,17 +1,38 @@
 import { StyledContList, StyledItem, StyledButton } from './Contacts.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact, getContacts, getFilter } from 'redux/contactSlice';
 import PropTypes from 'prop-types';
 
-export const Contacts = ({ contacts, onDelete }) => {
+export const Contacts = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const handleDelete = e => {
+    const chengedContacts = contacts.filter(
+      contact => contact.id !== e.currentTarget.parentElement.id
+    );
+    dispatch(deleteContact(chengedContacts));
+  };
+
+  const filteredContacts = contacts.filter(contact => {
+    return contact.name
+      .toLocaleLowerCase()
+      .includes(filter.toLocaleLowerCase());
+  });
+
+  const shownContacts = filter !== '' ? filteredContacts : contacts;
+
   return (
     <StyledContList>
-      {contacts.map(contact => {
+      {shownContacts.map(contact => {
         return (
           <StyledItem key={contact.id} id={contact.id}>
             <div>
               <span>{contact.name}</span>
               <span>{contact.number}</span>
             </div>
-            <StyledButton type="button" onClick={onDelete}>
+            <StyledButton type="button" onClick={handleDelete}>
               delete
             </StyledButton>
           </StyledItem>
